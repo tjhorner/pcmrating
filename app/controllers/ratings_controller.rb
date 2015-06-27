4972fa97417168853e4410ba649347612d5ee4b8
Daniel Lockhart <daniel@lockyy.com>
2015-06-27 03:29:27 +0100

    Improve navigation around the site

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -44,7 +44,7 @@ class RatingsController < ApplicationController
   private
 
     def user?
-      redirect_to root_path unless current_user
+      redirect_to new_user_session_path unless current_user
     end
 
     def admin?
