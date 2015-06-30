3c2df41909f6d6a23df84ae96765d913b97f70c0
Daniel Lockhart <daniel@lockyy.com>
2015-07-01 00:28:26 +0100

    Add slugs to existing games

--- db/migrate/20150628235124_add_unique_slug_to_game.rb
+++ db/migrate/20150628235124_add_unique_slug_to_game.rb
@@ -2,5 +2,7 @@ class AddUniqueSlugToGame < ActiveRecord::Migration
   def change
     add_column :games, :slug, :string, unique: true
     add_index :games, :slug, unique: true
+
+    Game.find_each(&:save)
   end
 end
